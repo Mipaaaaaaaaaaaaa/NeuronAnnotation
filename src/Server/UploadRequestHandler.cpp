260860274025671a099d9c26de0e3b3d009fba40
xzx1449 <1449676107@qq.com>
2021-05-17 05:04:36 +0800

    添加了upload功能

--- src/Server/UploadRequestHandler.cpp
+++ src/Server/UploadRequestHandler.cpp
@@ -7,6 +7,10 @@
 #include <Poco/Net/NetException.h>
 #include <Poco/Net/WebSocket.h>
 #include <Poco/Util/Application.h>
+#include <Poco/StreamCopier.h>
+#include <Poco/Net/PartHandler.h>
+#include<Poco/Net/MessageHeader.h>
+#include<Poco/Net/HTMLForm.h>
 #include <seria/deserialize.hpp>
 #include <iostream>
 #include <AnnotationDS.hpp>
@@ -15,25 +19,84 @@
 #include <DataBase.hpp>
 #include <iostream>
 using Poco::Util::Application;
-using WebSocket = Poco::Net::WebSocket;
+
+class MyPartHandler : public Poco::Net::PartHandler{
+public:
+    void handlePart(const Poco::Net::MessageHeader &header,std::istream &stream){
+        if (header.has("Content-Disposition")){
+            std::string disp;
+            Poco::Net::NameValueCollection params;
+            Poco::Net::MessageHeader::splitParameters(header["Content-Disposition"], disp, params);
+            _disp = header["Content-Disposition"];
+            _type = header["Content-Type"];
+            _name = params.get("name", "(unnamed)");
+            _filename = params.get("filename", "(unnamed)");
+            Poco::StreamCopier::copyToString(stream, _buffer);
+            _length = _buffer.size();
+        }
+    }
+    string buffer(){
+        return _buffer;
+    }
+    string filename(){
+        return _filename;
+    }
+    string name(){
+        return _name;
+    }
+    int length(){
+        return _length;
+    }
+private:
+    int _length;
+    std::string _disp;
+    std::string _type;
+    std::string _name;
+    std::string _filename;
+    std::string _buffer;
+};
 
 void UploadRequestHandler::handleRequest(
     Poco::Net::HTTPServerRequest &request,
     Poco::Net::HTTPServerResponse &response) {
   
     Application &app = Application::instance();
-    
-    try
+    std::cout << request.getMethod() << endl;
+    response.add("Access-Control-Allow-Origin", "*");
+    response.add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    response.add("Access-Control-Max-Age", "3600");
+    response.add("Access-Control-Allow-Headers", "x-requested-with,authorization");
+    if (request.getMethod() == "OPTIONS")
     {
-        char buffer[4096];
-        int flags = 0;
+        response.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
+        response.send();
+        return;
+    }
+    int len = request.getContentLength();
+    std::istream &i = request.stream();
+    MyPartHandler partHandler;
+    Poco::Net::HTMLForm form(request, request.stream(), partHandler);
+    std::cout << "buffer:" << partHandler.buffer() << endl;
+    std::cout << "file name:" << partHandler.filename() << endl;
+    std::cout << "file size:" << partHandler.length() << endl;
+    response.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
+    response.send();
+    return;
 
-        std::istream &i = request.stream();
-        int len = request.getContentLength();
-        i.read(buffer, len);
+
+
+    // try
+    // {
+
+        // char buffer[4096];
+        // int flags = 0;
+
+        // std::istream &i = request.stream();
+        // int len = request.getContentLength();
+        // i.read(buffer, len);
         
-        std::cout <<request.getContentType() << std::endl;
-        std::cout << buffer << std::endl;
+        // std::cout <<request.getContentType() << std::endl;
+        // std::cout << buffer << std::endl;
     //     auto one_hour = Poco::Timespan(0, 1, 0, 0, 0);
     //     ws.setReceiveTimeout(one_hour);
     //     rapidjson::Document document{};
@@ -57,23 +120,23 @@ void UploadRequestHandler::handleRequest(
     //             ws.sendFrame(error.what(), std::strlen(error.what()),WebSocket::FRAME_TEXT);
     //         }
     //     }while(len>0 && (flags & WebSocket::FRAME_OP_BITMASK) !=WebSocket::FRAME_OP_CLOSE);
-    }//try
-    catch (Poco::Net::WebSocketException &exc)
-    {
+    // }//try
+    // catch (Poco::Net::WebSocketException &exc)
+    // {
 
-        app.logger().log(exc);
-        switch (exc.code())
-        {
-            case WebSocket::WS_ERR_HANDSHAKE_UNSUPPORTED_VERSION:
-                response.set("Sec-WebSocket-Version", WebSocket::WEBSOCKET_VERSION);
-                // fallthrough
-            case WebSocket::WS_ERR_NO_HANDSHAKE:
-            case WebSocket::WS_ERR_HANDSHAKE_NO_VERSION:
-            case WebSocket::WS_ERR_HANDSHAKE_NO_KEY:
-                response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
-                response.setContentLength(0);
-                response.send();
-                break;
-        }//switch
-    }//catch
+    //     app.logger().log(exc);
+    //     switch (exc.code())
+    //     {
+    //         case WebSocket::WS_ERR_HANDSHAKE_UNSUPPORTED_VERSION:
+    //             response.set("Sec-WebSocket-Version", WebSocket::WEBSOCKET_VERSION);
+    //             // fallthrough
+    //         case WebSocket::WS_ERR_NO_HANDSHAKE:
+    //         case WebSocket::WS_ERR_HANDSHAKE_NO_VERSION:
+    //         case WebSocket::WS_ERR_HANDSHAKE_NO_KEY:
+    //             response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_BAD_REQUEST);
+    //             response.setContentLength(0);
+    //             response.send();
+    //             break;
+    //     }//switch
+    // }//catch
 }
